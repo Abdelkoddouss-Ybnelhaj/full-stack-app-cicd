16e00f808abf15be326d6566c30ce17a37f2d104
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-03-08 23:43:56 +0000

    update variables

--- infra/terraform/variable.tf
+++ infra/terraform/variable.tf
@@ -1,12 +1,17 @@
-
-variable "gcp_svc_key_path" {
-  
-}
-
 variable "gcp_project" {
   
 }
 
 variable "gcp_region" {
   
+}
+
+variable "ssh_public_key" {
+  description = "Public SSH key for authentication"
+  type        = string
+}
+
+variable "gcp_svc_key_path" {
+  description = "Path to Google Cloud service account key file"
+  type        = string
 }
